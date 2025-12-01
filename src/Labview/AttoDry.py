26af9d222aebc86f46ac6a123fce578af3cdfcd3
Gianpaul Rincon <164666509+pandamec@users.noreply.github.com>
2025-12-01 15:41:06 +0100

    einfache Regelung in Labview

--- src/Labview/AttoDry.py
+++ src/Labview/AttoDry.py
@@ -1,13 +1,37 @@
+import sys
+sys.path.append(r"E:\00 Script\Attodry800.Characterization\src")
+import Atto800
+
+IP = "192.168.1.1"
+Atto=Atto800.Device(IP)
+Atto.connect()
+
+
+
+def initiateAttoDry():
+
+    return 1
+
+def startCoolingDown():
+    Atto.action.goToBase()
+
 def startSamplePlate(T):
-    Atto.sample.startTempControl(T)
+    Atto.sample.setSetPoint(T)
+    Atto.sample.startTempControl()
+
 
 def startColdPlate(T):
     Atto.exchange.setSetPoint(T)
-
-def stopControl():
-    Atto.sample.startTempControl()
     Atto.exchange.startTempControl()
 
-def getSampleTemperature():
-    Atto.sample.getTemperature()
+def stopControl():
+    Atto.sample.stopTempControl()
+    Atto.exchange.stopTempControl()
 
+def getSampleTemperature():
+    T=Atto.sample.getTemperature()
+    return T
+
+def getColdPlateTemperature():
+    T=Atto.exchange.getTemperature()
+    return T
